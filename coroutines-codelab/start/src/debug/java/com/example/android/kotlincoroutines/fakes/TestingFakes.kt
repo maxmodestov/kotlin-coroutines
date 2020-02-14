9750456c6db58733f90c7798d52f7f6d90c25413
Maxim Modestov <maks.modestov@gmail.com>
2020-02-14 01:10:15 +0100

    Replace with suspend calls of Room and Retrofit functions

--- coroutines-codelab/start/src/debug/java/com/example/android/kotlincoroutines/fakes/TestingFakes.kt
+++ coroutines-codelab/start/src/debug/java/com/example/android/kotlincoroutines/fakes/TestingFakes.kt
@@ -45,7 +45,7 @@ class TitleDaoFake(initialTitle: String) : TitleDao {
      */
     private val insertedForNext = Channel<Title>(capacity = Channel.BUFFERED)
 
-    override fun insertTitle(title: Title) {
+    override suspend fun insertTitle(title: Title) {
         insertedForNext.offer(title)
         _titleLiveData.value = title
     }
@@ -91,7 +91,7 @@ class TitleDaoFake(initialTitle: String) : TitleDao {
  * Testing Fake implementation of MainNetwork
  */
 class MainNetworkFake(var result: String) : MainNetwork {
-    override fun fetchNextTitle() = MakeCompilerHappyForStarterCode() // TODO: replace with `result`
+    override suspend fun fetchNextTitle() = result
 }
 
 /**
@@ -100,7 +100,7 @@ class MainNetworkFake(var result: String) : MainNetwork {
 class MainNetworkCompletableFake() : MainNetwork {
     private var completable = CompletableDeferred<String>()
 
-    override fun fetchNextTitle() = MakeCompilerHappyForStarterCode() // TODO: replace with `completable.await()`
+    override suspend fun fetchNextTitle() = completable.await()
 
     fun sendCompletionToAllCurrentRequests(result: String) {
         completable.complete(result)
@@ -110,39 +110,6 @@ class MainNetworkCompletableFake() : MainNetwork {
     fun sendErrorToCurrentRequests(throwable: Throwable) {
         completable.completeExceptionally(throwable)
         completable = CompletableDeferred()
-    }
-
-}
-
-typealias MakeCompilerHappyForStarterCode = FakeCallForRetrofit<String>
-
-/**
- * This class only exists to make the starter code compile. Remove after refactoring retrofit to use
- * suspend functions.
- */
-class FakeCallForRetrofit<T> : Call<T> {
-    override fun enqueue(callback: Callback<T>) {
-        // nothing
-    }
-
-    override fun isExecuted() = false
-
-    override fun clone(): Call<T> {
-        return this
-    }
-
-    override fun isCanceled() = true
-
-    override fun cancel() {
-        // nothing
-    }
-
-    override fun execute(): Response<T> {
-        TODO("Not implemented")
-    }
-
-    override fun request(): Request {
-        TODO("Not implemented")
     }
 
 }
