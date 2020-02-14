92132453530b0e91fd3a7198f27e634e01ceacc1
Maxim Modestov <maks.modestov@gmail.com>
2020-02-14 01:31:59 +0100

    Remove unused imports

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -18,10 +18,7 @@ package com.example.android.kotlincoroutines.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
-import com.example.android.kotlincoroutines.util.BACKGROUND
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.withTimeout
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
